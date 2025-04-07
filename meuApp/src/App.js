7ca571b4fb758234b83d1cf9121c313deb6a2a04
MariaMendes11 <mcmendes900@gmail.com>
2025-04-07 16:11:05 -0300

    07-04

--- meuApp/src/App.js
+++ meuApp/src/App.js
@@ -10,13 +10,17 @@ import CadastroIngressoScreen from './screens/CadastroIngressoScreen';
 import Layout from "./components/Layout";
 import Login from "./screens/LoginScreen";
 import Cadastro from "./screens/CadastroScreen"
+import TaskList from './screens/TaskList';
+import TaskDetail from './screens/TaskDetail';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator >
+      <Stack.Navigator>
+        <Stack.Screen name="TaskList"component={TaskList}/>
+        <Stack.Screen name="TaskDetail"component={TaskDetail}/>
         <Stack.Screen 
           name="Login" 
           component={()=>(

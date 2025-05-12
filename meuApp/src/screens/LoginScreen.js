ff775303220dd8f8fbffd35acf9e906f66a055fe
MariaMendes11 <mcmendes900@gmail.com>
2025-05-12 16:14:28 -0300

    token

--- meuApp/src/screens/LoginScreen.js
+++ meuApp/src/screens/LoginScreen.js
@@ -1,97 +1,114 @@
-import React, {useState} from "react";
-import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Button } from "react-native";
-import api from '../axios/axios'
-import {Ionicons} from '@expo/vector-icons';
-import { useNavigation } from "@react-navigation/native"; 
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  StyleSheet,
+  Button,
+} from "react-native";
+import api from "../axios/axios";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import * as SecureStore from "expo-secure-store";
 
+export default function Login() {
+  const navigation = useNavigation();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
 
-export default function Login(){
-    const navigation = useNavigation();
-    const [user,setUser] = useState({
-        email:"",
-        password:"",
-        showPassword:false
-    })
+  async function saveToken(token) {
+    await SecureStore.setItemAsync("token", token);
+    console.log(token);
+  }
 
-    async function handleLogin() {
-        console.log(user)
-        await api.postLogin(user).then(
-            (response)=>{
-                console.log(response.data.message)
-                Alert.alert('OK',response.data.message)
-                navigation.navigate("EventoScreen")
-            },(error)=>{
-                    Alert.alert('Erro',error.response.data.error)
-                    console.log(error)
-            }
-        )
-    }
+  async function handleLogin() {
+    await api.postLogin(user).then(
+      (response) => {
+        Alert.alert("OK", response.data.message);
+        saveToken(response.data.token);
+        navigation.navigate("EventoScreen");
+      },
+      (error) => {
+        Alert.alert("Erro", error.response.data.error);
+        console.log(error);
+      }
+    );
+  }
 
-    return(
-        <View style={styles.container}>
-        <Text style={styles.title}>Faça Login</Text>
-        <TextInput
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Faça Login</Text>
+      <TextInput
         style={styles.input}
         placeholder="Email"
         value={user.email}
-        onChangeText={(value)=> {setUser({ ...user, email: value});
+        onChangeText={(value) => {
+          setUser({ ...user, email: value });
         }}
-        />
-        <View style={styles.passwordContainer}>
+      />
+      <View style={styles.passwordContainer}>
         <TextInput
-        style={styles.passwordInput}
-        placeholder="Senha"
-        value={user.password}
-        secureTextEntry={user.showPassword}
-        onChangeText={(value)=> {setUser({ ...user, password: value});
-        }}
+          style={styles.passwordInput}
+          placeholder="Senha"
+          value={user.password}
+          secureTextEntry={user.showPassword}
+          onChangeText={(value) => {
+            setUser({ ...user, password: value });
+          }}
         />
-        <TouchableOpacity onPress={()=> setUser({...user, showPassword: !user.showPassword })}>
-          <Ionicons name={user.showPassword?"eye-off":"eye"} size={24}/>
+        <TouchableOpacity
+          onPress={() => setUser({ ...user, showPassword: !user.showPassword })}
+        >
+          <Ionicons name={user.showPassword ? "eye-off" : "eye"} size={24} />
         </TouchableOpacity>
-        </View>
-        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+      </View>
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text style={styles.buttonText}>Entrar</Text>
       </TouchableOpacity>
       <Button
         title="Cadastro"
         onPress={() => navigation.navigate("Cadastro")}
       />
-        </View>
-    )
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems:'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
-  title:{
-    fontSize:28,
-    fontWeight:'bold'
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
   },
-  input:{
-    width:'100%',
-    height:40,
-    borderBottomWidth:1,
-    marginBotton:'20',
-    paddingHorizontal:'10'
+  input: {
+    width: "100%",
+    height: 40,
+    borderBottomWidth: 1,
+    marginBotton: "20",
+    paddingHorizontal: "10",
   },
-  button:{
-    backgroundColor:'purple',
-    padding:10,
-    borderRadius:5
+  button: {
+    backgroundColor: "purple",
+    padding: 10,
+    borderRadius: 5,
   },
-  passwordContainer:{
-    flexDirection:"row",
-    alignItems:"center",
-    width:"100%",
-    borderBottomWidth:1,
-    paddingRight: 10
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    borderBottomWidth: 1,
+    paddingRight: 10,
   },
-  passwordInput:{
-    flex:1,
-    height:40
-  }
+  passwordInput: {
+    flex: 1,
+    height: 40,
+  },
 });
